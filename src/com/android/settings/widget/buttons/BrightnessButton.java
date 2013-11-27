ca880d93209b3ad3e4746a2ee5cb6335a1d2203f
dimacn <dimacn@ukr.net>
2013-11-27 12:24:23 +0200

    Smooth backlight switching
    Change-Id: I8d04299e366562f6a42c7678f801ee7e185a6b9d

--- src/com/android/settings/widget/buttons/BrightnessButton.java
+++ src/com/android/settings/widget/buttons/BrightnessButton.java
@@ -19,13 +19,13 @@ public class BrightnessButton extends WidgetButton {
 	 * Minimum and maximum brightnesses. Don't go to 0 since that makes the
 	 * display unusable
 	 */
-	private static final int MINIMUM_BACKLIGHT = android.os.Power.BRIGHTNESS_DIM;
-	private static final int MAXIMUM_BACKLIGHT = android.os.Power.BRIGHTNESS_ON;
-	private static int DEFAULT_BACKLIGHT = (int) (android.os.Power.BRIGHTNESS_ON * 0.4f);
+        private static final int MINIMUM_BACKLIGHT = android.os.Power.BRIGHTNESS_DIM;
+        private static final int MAXIMUM_BACKLIGHT = android.os.Power.BRIGHTNESS_ON;
+        private static int DEFAULT_BACKLIGHT = (int) (android.os.Power.BRIGHTNESS_ON * 0.25f);
 
-	private static int LOW_BACKLIGHT = (int) (android.os.Power.BRIGHTNESS_ON * 0.25f);
-	private static int MID_BACKLIGHT = (int) (android.os.Power.BRIGHTNESS_ON * 0.5f);
-	private static int HIGH_BACKLIGHT = (int) (android.os.Power.BRIGHTNESS_ON * 0.75f);
+        private static int BACKLIGHT_1 = (int) (android.os.Power.BRIGHTNESS_ON * 0.06f);
+        private static int BACKLIGHT_2 = (int) (android.os.Power.BRIGHTNESS_ON * 0.2f);
+        private static int BACKLIGHT_3 = (int) (android.os.Power.BRIGHTNESS_ON * 0.45f);
 
 	private static final int AUTO_BACKLIGHT = -1;
 
@@ -109,60 +109,45 @@ Low High Max
 	}
 
 
+    private int getNextBrightnessValue(Context context) {
+        int brightness = Settings.System.getInt(context.getContentResolver(),
+                Settings.System.SCREEN_BRIGHTNESS,0);
 
-	private int getNextBrightnessValue(Context context) {
-		int brightness = Settings.System.getInt(context.getContentResolver(),
-				Settings.System.SCREEN_BRIGHTNESS,0);
-		/*
-	private static final int MODE_AUTO_MIN_DEF_MAX=0;
-	private static final int MODE_AUTO_MIN_LOW_MID_HIGH_MAX=1;
-	private static final int MODE_AUTO_LOW_MAX=2;
-		 * 
-		 */
-		if (isAutomaticModeSupported(context) && isBrightnessSetToAutomatic(context)) {
-			if (currentMode==MODE_AUTO_LOW_MAX) {
-				return LOW_BACKLIGHT;
-			} else {
-				return getMinBacklight(context);				
-			}
-		} else if (brightness < LOW_BACKLIGHT) {
-			if (currentMode==MODE_AUTO_LOW_MAX) {
-				return LOW_BACKLIGHT;
-			} else if (currentMode==MODE_MIN_MAX) {
-				return MAXIMUM_BACKLIGHT;
-			} else {
-				return DEFAULT_BACKLIGHT;				
-			}			
-		} else if (brightness < DEFAULT_BACKLIGHT) {
-			if (currentMode==MODE_AUTO_MIN_DEF_MAX) {
-				return DEFAULT_BACKLIGHT;
-			} else if (currentMode==MODE_AUTO_LOW_MAX || currentMode==MODE_MIN_MAX) {
-				return MAXIMUM_BACKLIGHT;
-			} else {
-				return MID_BACKLIGHT;				
-			}			
-		} else if (brightness < MID_BACKLIGHT) {
-			if (currentMode==MODE_AUTO_MIN_LOW_MID_HIGH_MAX) {
-				return MID_BACKLIGHT;
-			} else {
-				return MAXIMUM_BACKLIGHT;
-			}			
-		} else if (brightness < HIGH_BACKLIGHT) {
-			if (currentMode==MODE_AUTO_MIN_LOW_MID_HIGH_MAX) {
-				return HIGH_BACKLIGHT;
-			} else {
-				return MAXIMUM_BACKLIGHT;
-			}			
-		} else if (brightness < MAXIMUM_BACKLIGHT) {
-			return MAXIMUM_BACKLIGHT;
-		} else if (isAutomaticModeSupported(context) && currentMode!=MODE_MIN_MAX) {
-			return AUTO_BACKLIGHT;
-		} else if(currentMode == MODE_AUTO_LOW_MAX){
-			return LOW_BACKLIGHT;				
-		} else {
-			return getMinBacklight(context);
-		}
-	}
+        if (isAutomaticModeSupported(context) && isBrightnessSetToAutomatic(context)) {
+            if (currentMode == MODE_AUTO_LOW_MAX) {
+                return DEFAULT_BACKLIGHT;
+            } else {
+                return getMinBacklight(context);
+            }
+        } else if (currentMode == MODE_AUTO_MIN_LOW_MID_HIGH_MAX) {
+            if (brightness < BACKLIGHT_1) {
+                return BACKLIGHT_1;
+            } else if (brightness < BACKLIGHT_2) {
+                return BACKLIGHT_2;
+            } else if (brightness < BACKLIGHT_3) {
+                return BACKLIGHT_3;
+            } else if (brightness < MAXIMUM_BACKLIGHT) {
+                return MAXIMUM_BACKLIGHT;
+            } else {
+                return MINIMUM_BACKLIGHT;
+            }
+        } else if (brightness < DEFAULT_BACKLIGHT) {
+            if (currentMode == MODE_MIN_MAX) {
+                return MAXIMUM_BACKLIGHT;
+            } else {
+                return DEFAULT_BACKLIGHT;
+            }
+        } else if (brightness < MAXIMUM_BACKLIGHT) {
+            return MAXIMUM_BACKLIGHT;
+        } else if (isAutomaticModeSupported(context) && currentMode!=MODE_MIN_MAX) {
+            return AUTO_BACKLIGHT;
+        } else if(currentMode == MODE_AUTO_LOW_MAX){
+            return DEFAULT_BACKLIGHT;
+        } else {
+            return getMinBacklight(context);
+        }
+    }
+
 
 	/**
 	 * Increases or decreases the brightness.
@@ -236,30 +221,31 @@ Low High Max
 		}
 	}
 
-	private int getBrightnessState(Context context) {
-		int brightness = Settings.System.getInt(context.getContentResolver(),
-				Settings.System.SCREEN_BRIGHTNESS,0);
+    private int getBrightnessState(Context context) {
+        int brightness = Settings.System.getInt(context.getContentResolver(),
+                Settings.System.SCREEN_BRIGHTNESS,0);
 
-		if (brightness < LOW_BACKLIGHT) {
-			return SettingsAppWidgetProvider.STATE_DISABLED;
-		} else if (brightness < DEFAULT_BACKLIGHT) {
-			return SettingsAppWidgetProvider.STATE_DISABLED;
-		} else if (brightness < MID_BACKLIGHT) {
-			if (currentMode==MODE_AUTO_MIN_LOW_MID_HIGH_MAX) {
-				return SettingsAppWidgetProvider.STATE_DISABLED;
-			} else {
-				return SettingsAppWidgetProvider.STATE_TURNING_OFF;
-			}			
-		} else if (brightness < HIGH_BACKLIGHT) {
-			if (currentMode==MODE_AUTO_MIN_LOW_MID_HIGH_MAX) {
-				return SettingsAppWidgetProvider.STATE_TURNING_OFF;
-			} else {
-				return SettingsAppWidgetProvider.STATE_TURNING_ON;
-			}			
-		} else if (brightness < MAXIMUM_BACKLIGHT) {
-			return SettingsAppWidgetProvider.STATE_TURNING_ON;
-		} else {
-			return SettingsAppWidgetProvider.STATE_ENABLED;
-		}		
-	}
+        if (currentMode == MODE_AUTO_MIN_LOW_MID_HIGH_MAX) {
+            if (brightness < BACKLIGHT_2) {
+                return SettingsAppWidgetProvider.STATE_DISABLED;
+            } else if (brightness < BACKLIGHT_3) {
+                return SettingsAppWidgetProvider.STATE_TURNING_OFF;
+            } else if (brightness < MAXIMUM_BACKLIGHT) {
+                return SettingsAppWidgetProvider.STATE_TURNING_ON;
+            } else {
+                return SettingsAppWidgetProvider.STATE_ENABLED;
+            }
+        } else if (brightness < DEFAULT_BACKLIGHT) {
+            return SettingsAppWidgetProvider.STATE_DISABLED;
+        } else if (brightness < MAXIMUM_BACKLIGHT) {
+            if (currentMode == MODE_AUTO_LOW_MAX) {
+                return SettingsAppWidgetProvider.STATE_DISABLED;
+            } else {
+                return SettingsAppWidgetProvider.STATE_TURNING_ON;
+            }
+        } else {
+            return SettingsAppWidgetProvider.STATE_ENABLED;
+        }
+    }
+
 }
